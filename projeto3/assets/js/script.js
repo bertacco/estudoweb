65acaf0705f3b17bb32a5ba36627634be2a064a7
bertacco <bertaccode@gmail.com>
2019-09-25 15:37:41 -0300

    setas de navegaçaõ

--- projeto3/assets/js/script.js
+++ projeto3/assets/js/script.js
@@ -1,4 +1,23 @@
 
+function avançar() {
+	var slideAtivo = document.querySelector('.slide.ativo');
+	var proximo = slideAtivo.nextElementSibling;
+	if (proximo!=null) {
+		slideAtivo.classList.remove("ativo");
+		proximo.classList.add("ativo");
+	}
+}
+
+function retroceder() {
+	var slideAtivo = document.querySelector('.slide.ativo');
+	var proximo = slideAtivo.previousElementSibling;
+	if (proximo!=null) {
+		slideAtivo.classList.remove("ativo");
+		proximo.classList.add("ativo");
+	}
+	
+}
+
 setInterval (function(){
 	var slideAtivo = document.querySelector('.slide.ativo');
 	var proximo = slideAtivo.nextElementSibling;
@@ -9,4 +28,4 @@ setInterval (function(){
 	proximo.classList.add("ativo");
 
 
-},3000)
+},3000);
